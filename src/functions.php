2c4c57637f3a014ba0503610861d11a0eaa48833
Christian Lück <christian@lueck.tv>
2015-07-01 20:46:41 +0200

    Use newer Promise API instead of legacy Deferreds Closes #4

--- src/functions.php
+++ src/functions.php
@@ -2,43 +2,39 @@
 
 namespace Clue\Promise\Timeout;
 
-use React\Promise\Deferred;
 use React\Promise\CancellablePromiseInterface;
 use React\EventLoop\LoopInterface;
 use React\Promise\PromiseInterface;
+use React\Promise\Promise;
 
 function await(PromiseInterface $promise, $time, LoopInterface $loop)
 {
-    $deferred = new Deferred();
+    return new Promise(function ($resolve, $reject) use ($loop, $time, $promise) {
+        $timer = $loop->addTimer($time, function () use ($time, $promise, $reject) {
+            $reject(new TimeoutException($time, 'Timed out after ' . $time . ' seconds'));
 
-    $timer = $loop->addTimer($time, function () use ($time, $promise, $deferred) {
-        $deferred->reject(new TimeoutException($time, 'Timed out after ' . $time . ' seconds'));
+            if ($promise instanceof CancellablePromiseInterface) {
+                $promise->cancel();
+            }
+        });
 
-        if ($promise instanceof CancellablePromiseInterface) {
-            $promise->cancel();
-        }
+        $promise->then(function ($v) use ($timer, $loop, $resolve) {
+            $loop->cancelTimer($timer);
+            $resolve($v);
+        }, function ($v) use ($timer, $loop, $reject) {
+            $loop->cancelTimer($timer);
+            $reject($v);
+        });
     });
-
-    $promise->then(function ($v) use ($timer, $loop, $deferred) {
-        $loop->cancelTimer($timer);
-        $deferred->resolve($v);
-    }, function ($v) use ($timer, $loop, $deferred) {
-        $loop->cancelTimer($timer);
-        $deferred->reject($v);
-    });
-
-    return $deferred->promise();
 }
 
 function resolve($time, LoopInterface $loop)
 {
-    $deferred = new Deferred();
-
-    $timer = $loop->addTimer($time, function () use ($time, $deferred) {
-        $deferred->resolve($time);
+    return new Promise(function ($resolve) use ($loop, $time) {
+        $loop->addTimer($time, function () use ($time, $resolve) {
+            $resolve($time);
+        });
     });
-
-    return $deferred->promise();
 }
 
 function reject($time, LoopInterface $loop)
